6d848fa6226ae1a01c7e756254bd3f2e7506809c
Johnisonn <151909152+Johnisonn@users.noreply.github.com>
2025-01-02 21:52:51 +0800

    Create main.py

---
+++ fuction/main.py
@@ -0,0 +1,13 @@
+
+import logging
+
+from test import x
+
+logging.basicConfig(level=logging.INFO, datefmt='%Y-%m_%d %H:%M:%S %p', format='%(asctime)s-%(levelname)s-%(name)s-%(message)s', handlers=[logging.FileHandler(filename='project.log', mode='w'), logging.StreamHandler()])
+logger = logging.getLogger(__name__)
+
+for i in range(20):
+    b = x(i)
+    with open('../平方.txt', 'w', encoding='utf-8') as f:
+        f.write(f'{b}')
+        logger.info(f'{i}的平方值已写入文件')
