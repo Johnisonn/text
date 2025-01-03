038736b4bbb3adf6324044a1554c266f2e77fa90
Johnisonn <151909152+Johnisonn@users.noreply.github.com>
2025-01-03 16:08:12 +0800

    Update main.py

--- fuction/main.py
+++ fuction/main.py
@@ -6,8 +6,8 @@ from test import x
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m_%d %H:%M:%S %p', format='%(asctime)s-%(levelname)s-%(name)s-%(message)s', handlers=[logging.FileHandler(filename='project.log', mode='w'), logging.StreamHandler()])
 logger = logging.getLogger(__name__)
 
-    with open('平方.txt', 'w', encoding='utf-8') as f:
-        for i in range(20):
-            b = x(i)
-            f.write(f'{b}')
-            logger.info(f'{i}的平方值已写入文件')
+with open('平方.txt', 'w', encoding='utf-8') as f:
+    for i in range(20):
+        b = x(i)
+        f.write(f'{b}')
+        logger.info(f'{i}的平方值已写入文件')
